22e97e9b9f32ce6736e8e051dc20fdfbfe8cab9e
gdan <daliwong@protonmail.com>
2025-04-04 22:47:40 -0400

    fixed sizes loading vector

--- simd-learn/main.cpp
+++ simd-learn/main.cpp
@@ -26,24 +26,56 @@ bool memset_simd( int8* a_pData, const char a_ch, size_t a_sz )
       return false;
    }
 
-   char   *pdata = (char*)a_pData;
+   //char   *pdata = (char*)a_pData;
+   __m256i *p256 = &(a_pData->v);
    size_t  sz = a_sz;
 
    __m256i vstr = _mm256_set1_epi8( a_ch );
    while( sz > 0 )
    {
-     _mm256_storeu_si256( (__m256i*)pdata, vstr );
-     //_mm256_stream_si256( (__m256i*)pdata, vstr );
+     //_mm256_storeu_si256( (__m256i*)pdata, vstr );
+     _mm256_storeu_si256( p256, vstr );
 
      // *(int64_t*)(pdata+0)  = vstr[0];
      // *(int64_t*)(pdata+8)  = vstr[1];
      // *(int64_t*)(pdata+16) = vstr[2];
      // *(int64_t*)(pdata+24) = vstr[3];
 
-      pdata += sizeof( __m256i );
+      //pdata += sizeof( __m256i );
+      //p256 += sizeof( __m256i );
+      ++p256;
       sz -= sizeof( __m256i );
    }
    return true;
+}
+
+void run_std( char* a_pdata, size_t a_szBuf, const int32_t a_count )
+{
+   char ch = 0;  // let it rollover
+
+   auto start_reg = std::chrono::high_resolution_clock::now();
+   for( int i=0; i<a_count; ++i )
+   {
+      memset( (void*)a_pdata, ++ch, a_szBuf*sizeof(char) );
+   } 
+   auto stop_reg = std::chrono::high_resolution_clock::now();
+   auto duration_reg = std::chrono::duration_cast<std::chrono::microseconds>( stop_reg - start_reg );
+   auto str = std::format( "{:L}", duration_reg );
+   std::cout << "Memset std  took " << str << std::endl;
+}
+
+void run_simd( char* a_pdata, size_t a_szBuf, int32_t a_count )
+{
+   char ch = 0xFE;  // let it rollover, keep changing so that we get cache miss
+   auto start_simd = std::chrono::high_resolution_clock::now();
+   for( int i=0; i<a_count; ++i )
+   {
+      memset_simd( (int8*)a_pdata, ++ch, a_szBuf*sizeof(char) );
+   } 
+   auto stop_simd = std::chrono::high_resolution_clock::now();
+   auto duration_simd = std::chrono::duration_cast<std::chrono::microseconds>( stop_simd - start_simd );
+   auto str = std::format( "{:L}", duration_simd );
+   std::cout << "Memset simd took " << str << std::endl;
 }
 
 void mem_unk()
@@ -56,38 +88,19 @@ void mem_unk()
       //                              a_pData[1], a_pData[0];
 }
 
-void run_std( char* a_pdata, size_t a_szBuf, const int32_t a_count )
-{
-   char ch = 0;
-   auto start_reg = std::chrono::high_resolution_clock::now();
-   for( int i=0; i<a_count; ++i )
-   {
-      memset( (int8*)a_pdata, ch, a_szBuf*sizeof(char) );
-      ++ch;  // let it rollover
-   } 
-   auto stop_reg = std::chrono::high_resolution_clock::now();
-   auto duration_reg = std::chrono::duration_cast<std::chrono::microseconds>( stop_reg - start_reg );
-   auto str = std::format( "{:L}", duration_reg );
-   std::cout << "Memset std  took " << str << std::endl;
-}
-
-void run_simd( char* a_pdata, size_t a_szBuf, int32_t a_count )
-{
-   char ch = 0xFF;
-   auto start_simd = std::chrono::high_resolution_clock::now();
-   for( int i=0; i<a_count; ++i )
-   {
-      memset_simd( (int8*)a_pdata, ch, a_szBuf*sizeof(char) );
-      ++ch;  // let it rollover, keep changing so that we get cache miss
-   } 
-   auto stop_simd = std::chrono::high_resolution_clock::now();
-   auto duration_simd = std::chrono::duration_cast<std::chrono::microseconds>( stop_simd - start_simd );
-   auto str = std::format( "{:L}", duration_simd );
-   std::cout << "Memset simd took " << str << std::endl;
-}
 
 int main( int argc, char* argv[] )
 {
+   auto ptr = []( uint32_t a_nSz )  -> char*
+   {
+      if( (a_nSz % 32) != 0 )
+      {
+         std::cerr << "bad alloc" << std::endl;
+         return nullptr;
+      }
+      return new char[a_nSz];
+   };
+
    int32_t nRuns = 10000;
    if( argc > 1 )
    {
@@ -96,28 +109,27 @@ int main( int argc, char* argv[] )
 
    std::locale::global(std::locale("en_US.UTF-8"));
    char* pdata32 =  new char[32];
-   char* pdata64 =  new char[64];
-   char* pdata60 =  new char[60];
    int8 i8;
 
    memset_simd( (int8*)pdata32, 1, 32 );
-   memset_simd( (int8*)pdata64, 1, 64 );
-   memset_simd( (int8*)pdata60, 1, 60 );
+   memset_simd( (int8*)pdata32, 1, 30 );
    memset_simd( &i8, 1, sizeof(int8) );
    memset_simd( &i8, 0, sizeof(int8) );
 
    
    std::cout << "benchmark " << nRuns << " iterations" << std::endl;
 
-   size_t szBuf = 512*1024;
-   //size_t szBuf =  64;
-   char *pdata_simd = new char[szBuf];
-   char *pdata_std  = new char[szBuf];
-   char *pdata_align = nullptr;
-   posix_memalign( (void**)&pdata_align, sizeof( __m256i ), szBuf*sizeof(char) );
+   size_t szBuf = 512*1024*32;
+   //size_t szBuf = 100*32;
+   char *pdata_simd = ptr( szBuf );
+   char *pdata_std  = ptr( szBuf );
+   //char *pdata_align = nullptr;
+   //if( 0 != posix_memalign( (void**)&pdata_align, sizeof( __m256i ), szBuf*sizeof(char) ) )
+   //{
+   //   std::cerr << "posix memalign error" << std::endl;
+   //}
 
-   //run_simd( pdata_simd,szBuf, nRuns );
-   run_simd( pdata_align,szBuf, nRuns );
+   run_simd( pdata_simd,szBuf, nRuns );
    run_std ( pdata_std, szBuf, nRuns );
 
 
